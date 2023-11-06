631ae350ef34f36b71e871925007eda7b408bb0c
HarryNCY <76614495+HarryNCY@users.noreply.github.com>
2023-11-06 16:41:25 +0800

    feat: Add course card component

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,9 +1,18 @@
+import CourseCard from "./components/CourseCard";
+
 export default async function HomePage() {
     return (
         <div>
             This is home page.
             <br />
             You can only access this page if you are logged in.
+            <br />
+            <CourseCard
+                courseTitle="ENGG1330 Computer Programming I"
+                courseShortDescription="Python Introductory Course"
+                createdBy="Schnieders Dirk"
+                barColor="red"
+            />
         </div>
     );
 }
